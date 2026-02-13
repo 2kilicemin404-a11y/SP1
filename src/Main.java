a6a88e0975252b5c0768907352dfcd5cdc2d4ac3
eminkilic <kilicemin404@gmail.com>
2026-02-13 11:41:48 +0100

    I denne opgave har jeg lavet opgave 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,72 @@
-public class Main {
+
     void main () {
+//Opgave 1
+// Karakterens stats
+        String navn = "Ragnar";
+
+        int healthPoints = 85;
+
+        int maxHealth = 100;
+
+        int level = 5;
+
+        int experience = 2300;
+
+        double gold = 156.50;
+
+        boolean alive = true;
+
+        String klasse = "Warrior";
+
+//Items i karakterens inventory
+        String[] characterItems = {"Sword", "Dagger", "Shield"};
+
+
+        System.out.println("****Character****");
+        System.out.println("Navn: " + navn);
+        System.out.println("klasse: " + klasse);
+        System.out.println("Health: " + healthPoints + "/"+ maxHealth);
+        System.out.println("experience: " + experience);
+        System.out.println("lvl: " + level);
+        System.out.println("gold: " + gold);
+        System.out.println("alive: "+ alive);
+
+
+          String inventoryText ="- ";
+
+
+        for (int i= 0; i< characterItems.length; i++) {
+        inventoryText = inventoryText + characterItems[i];
+        inventoryText = inventoryText + "\n";
+
+        System.out.println("Items");
+        System.out.println(inventoryText);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
 
-    }
+
+
+
+
+
 
