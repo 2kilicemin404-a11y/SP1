80dd932420618f4dc240322932e11834ca0ff5c5
eminkilic <kilicemin404@gmail.com>
2026-02-14 00:50:14 +0100

    del 2.

--- src/Main.java
+++ src/Main.java
@@ -25,27 +25,57 @@
         System.out.println("****Character****");
         System.out.println("Navn: " + navn);
         System.out.println("klasse: " + klasse);
-        System.out.println("Health: " + healthPoints + "/"+ maxHealth);
+        System.out.println("Health: " + healthPoints + "/" + maxHealth);
         System.out.println("experience: " + experience);
         System.out.println("lvl: " + level);
         System.out.println("gold: " + gold);
-        System.out.println("alive: "+ alive);
+        System.out.println("alive: " + alive);
 
 
-          String inventoryText ="- ";
+        String inventoryText = "- ";
 
 
-        for (int i= 0; i< characterItems.length; i++) {
-        inventoryText = inventoryText + characterItems[i];
-        inventoryText = inventoryText + "\n";
+        for (int i = 0; i < characterItems.length; i++) {
+            inventoryText = inventoryText + characterItems[i];
+            inventoryText = inventoryText + "\n";
 
-        System.out.println("Items");
-        System.out.println(inventoryText);
+            System.out.println("Items");
+            System.out.println(inventoryText);
 
-            }
+        }
 
 
+        if (healthPoints <= 0.25 * maxHealth) {
+            System.out.println("WARNING: Health critical!");
+        } else if (healthPoints <= 0){
 
+            alive = false;
+            System.out.println("Alive: " + alive);
+        }
+
+        switch (klasse) {
+            case "Warrior":
+                System.out.println("I got a sword");
+                break;
+            case "Mage":
+                System.out.println("i got a wand");
+                break;
+            case "Rogue":
+                System.out.println("I got a dagger");
+                break;
+
+            default:
+                System.out.println("Classes: " );
+
+
+        }
+        if (experience >= 1000 * level){
+            System.out.println("Ready to level up!");
+        }else {
+            System.out.println("Not ready to level up!");
+
+
+        }
 
 
 
