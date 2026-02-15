a0baf594505c85301223c22254ae19836b960b2c
eminkilic <kilicemin404@gmail.com>
2026-02-15 16:32:06 +0100

    Metoder Healthcrticial og ALive er lavet.

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,92 @@
 
-    void main () {
-        String navn = "Ragnar";
+        void main () {
+//            String navn = "Ragnar";
 
-        int healthPoints = 85;
+//            int healthPoints = 85;
+//
+//            int maxHealth = 100;
+//
+//            int level = 5;
+//
+//            int experience = 2300;
+//
+//            double gold = 156.50;
+//
+//            boolean alive = true;
+//
+//            String klasse = "Warrior";
+//
+//            System.out.println("****Character****");
+//            System.out.println("Navn: " + navn);
+//            System.out.println("klasse: " + klasse);
+//            System.out.println("Health: " + healthPoints + "/" + maxHealth);
+//            System.out.println("experience: " + experience);
+//            System.out.println("lvl: " + level);
+//            System.out.println("gold: " + gold);
+//            System.out.println("alive: " + alive);
 
-        int maxHealth = 100;
+            String[] characterItems = {"-Sword", "-Dagger", "-Shield"};
 
-        int level = 5;
+            String inventoryText = " ";
 
-        int experience = 2300;
+            for (int i = 0; i < characterItems.length; i++) {
+                inventoryText = inventoryText + characterItems[i];
+                inventoryText = inventoryText + "\n";
 
-        double gold = 156.50;
+                System.out.println("Ragnars invetory:");
+                System.out.println(inventoryText);
 
-        boolean alive = true;
+            }
 
-        String klasse = "Warrior";
+//
+//            if (healthPoints <= 0.25 * maxHealth) {
+//                System.out.println("WARNING: Health critical!");
+//            } else if (healthPoints <= 0){
+//
+//                alive = false;
+//                System.out.println("Alive: " + alive);
+//            }
 
-        System.out.println("****Character****");
-        System.out.println("Navn: " + navn);
-        System.out.println("klasse: " + klasse);
-        System.out.println("Health: " + healthPoints + "/" + maxHealth);
-        System.out.println("experience: " + experience);
-        System.out.println("lvl: " + level);
-        System.out.println("gold: " + gold);
-        System.out.println("alive: " + alive);
+            switch (klasse) {
+                case "Warrior":
+                    System.out.println("Ragnar has a sword!");
+                    break;
+                case "Mage":
+                    System.out.println("Ragnar has a wand!");
+                    break;
+                case "Rogue":
+                    System.out.println("Ragnar has a dagger!");
+                    break;
 
-        String[] characterItems = {"-Sword", "-Dagger", "-Shield"};
+                default:
+                    System.out.println("Classes: " );
+            }
+//            if (experience >= 1000 * level){
+//                System.out.println("Ready to level up!");
+//            }else {
+//                System.out.println("Not ready to level up!");
 
-        String inventoryText = " ";
+            }
+            int numberOfItems = characterItems.length;
+            System.out.println("Invetory (items): = " + numberOfItems);
 
-        for (int i = 0; i < characterItems.length; i++) {
-            inventoryText = inventoryText + characterItems[i];
-            inventoryText = inventoryText + "\n";
-
-            System.out.println("Ragnars invetory:");
-            System.out.println(inventoryText);
-
-        }
-
-
-        if (healthPoints <= 0.25 * maxHealth) {
-            System.out.println("WARNING: Health critical!");
-        } else if (healthPoints <= 0){
-
-            alive = false;
-            System.out.println("Alive: " + alive);
-        }
-
-        switch (klasse) {
-            case "Warrior":
-                System.out.println("Ragnar has a sword!");
-                break;
-            case "Mage":
-                System.out.println("Ragnar has a wand!");
-                break;
-            case "Rogue":
-                System.out.println("Ragnar has a dagger!");
-                break;
-
-            default:
-                System.out.println("Classes: " );
-        }
-        if (experience >= 1000 * level){
-            System.out.println("Ready to level up!");
-        }else {
-            System.out.println("Not ready to level up!");
-
-        }
-        int numberOfItems = characterItems.length;
-        System.out.println("Invetory (items): = " + numberOfItems);
-
-        for (String item : characterItems){
-            System.out.println( item);
-        }
-
-        int incomingDamage = 30;
-
-        healthPoints = healthPoints - incomingDamage;
-        System.out.println("Ragnar took damage! new health: " + healthPoints);
-
-        if (healthPoints >0) {
-            System.out.println("Ragnar is still alive!");
-        }else{
-            System.out.println("Ragnar is dead! " + "Game over");
-        }
-
-
-    }
+            for (String item : characterItems){
+                System.out.println( item);
+            }
+//
+//            int incomingDamage = 30;
+//
+//            healthPoints = healthPoints - incomingDamage;
+//            System.out.println("Ragnar took damage! new health: " + healthPoints);
+//
+//            if (healthPoints >0) {
+//                System.out.println("Ragnar is still alive!");
+//            }else{
+//                System.out.println("Ragnar is dead! " + "Game over");
+//            }
+//
+//
+//        }
 
 
 
@@ -142,7 +142,7 @@
     void heal(int amount){
         healthPoints= healthPoints + amount;
 
-        if (healthPoints > maxHealth);{
+        if (healthPoints > maxHealth){
             healthPoints=maxHealth;
         }
         System.out.println(navn + " healed for " + amount + " HP.");
@@ -165,6 +165,46 @@ boolean removeGold(double amount){
 
         }
 }
-
+    void addXP(int amount){
+        experience = experience + amount;
+        if (experience >= 1000 * level){
+            System.out.println("Ready to level up!");
+        }else {
+            System.out.println("Not ready to level up!");
+        }
 
 }
+
+void levelUp(){
+level++;
+experience = 0;
+maxHealth = maxHealth +20;
+System.out.println("Level increased!");
+    System.out.println("Next level: " + level);
+    System.out.println("New max health: " + maxHealth);
+}
+
+boolean isHealthCritical() {
+    if (healthPoints <= 0.25 * maxHealth) {
+        return true;
+
+    } else {
+        return false;
+
+    }
+    }
+
+
+    boolean alive(){
+        if (healthPoints > 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    double getHealthPercentage(){
+        healthPoints = maxHealth/
+
+    }
